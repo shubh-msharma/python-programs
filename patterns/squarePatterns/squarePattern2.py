a1954165f3f9262796d2da22eea642756a10868b
shubh-msharma <shubhamsharma4539@gmail.com>
2020-06-12 17:36:11 +0530

    new program

---
+++ patterns/squarePatterns/squarePattern2.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+rNc = int(input("enter number of rows: "))
+for r in range(rNc):
+    # print(r,end=" ")
+    for c in range(rNc):
+        if r == 0 or r == rNc-1:
+            print("*" , end = " ")
+        elif c == 0 or c == rNc-1 :
+            print("*" , end = " ")
+        else :
+            print(" ",end=" ")
+    print()
