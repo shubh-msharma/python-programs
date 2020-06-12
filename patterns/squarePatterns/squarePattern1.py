c39dd5195261581dc6b3929ef9bb7571709838b0
shubh-msharma <shubhamsharma4539@gmail.com>
2020-06-12 17:16:56 +0530

    square pattern program mogified

---
+++ patterns/squarePatterns/squarePattern1.py
@@ -0,0 +1,12 @@
+
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+
+rNc = int(input("enter number fo rows: "))
+for x in range(rNc):
+    for y in range(rNc):
+        print("* ",end = "")
+    print()
